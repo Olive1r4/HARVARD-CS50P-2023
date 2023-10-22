dd2d02f8a00d637dccb519470b083e510306b878
Fernando Oliveira <fernandooliveira@live.com>
2023-10-22 20:25:11 +0000

    comentários

--- week_0/playback.py
+++ week_0/playback.py
@@ -1,8 +1,10 @@
+#Programa recebe uma string e substitui os espaços por três pontos
 def main():
     text = input("Digite o texto: ")
     print(f"{replace(text)}")
 
 def replace(t):
+    # retira os espaços antes de depois da string e substitui os espaços internos por três pontos
     return t.strip().replace(" ", "...")
 
 main()
